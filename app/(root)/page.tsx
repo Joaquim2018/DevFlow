87d5ebfb8211ce827094ac2b2797d8d11fb22b2c
Joaquim Munguambe <sinaiholdings@gmail.com>
2024-11-15 12:23:09 +0200

    NextAuth Layouts SocialAuthForm

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-black text-cyan-700">Hello world</h1>
+      <p className="text-lg text-purple-500">Joaquim</p>
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1 className="text-3xl font-black text-cyan-700">Hello world</h1>;
-}
